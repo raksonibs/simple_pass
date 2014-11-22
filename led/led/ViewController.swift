6859c50258985c62466993d9f1bff4126f207cd4
Oskar Niburski <oskarniburski@gmail.com>
2014-11-22 02:59:03 -0500

    LED files. Now building servo implementation

--- led/led/ViewController.swift
+++ led/led/ViewController.swift
@@ -10,24 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var reloadButton: UIButton!
+    let device_id = "53ff6b066667574852212367"
+    let access_token = "94f2788badb6634bc483fc1efe541e810280b9da"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // initial load
+        self.view.backgroundColor = UIColor(red: 0.145, green: 0.145, blue: 0.145, alpha: 1.0)
+        
+        let screenWidth = UIScreen.mainScreen().bounds.size.width
+        let screenHeight = UIScreen.mainScreen().bounds.size.height
+        
+        self.view.addSubview(createCircle(200, color:UIColor(red:0, green:0, blue:0, alpha: 1.0)))
+        self.view.addSubview(createCircle(195, color:UIColor(red: 0.145, green: 0.145, blue: 0.145, alpha: 1.0)))
+        self.view.addSubview(createCircle(185, color:UIColor(red:0, green:0, blue:0, alpha: 1.0)))
+        self.view.addSubview(createCircle(180, color:UIColor(red:0.808, green:0.906, blue:0.271, alpha:1.0)))
+        
+        let button = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+        button.frame = CGRectMake(
+            (screenWidth - 200) / 2.0,
+            (screenHeight - 200) / 2.0,
+            200,
+            200)
+        button.setTitle("Coming home!", forState: UIControlState.Normal)
+        button.titleLabel!.font = UIFont.boldSystemFontOfSize(22)
+        button.addTarget(self, action: "pressed:", forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
+        
     }
     
-    // when the reload button tapped
-    @IBAction func reloadDidTap(sender: AnyObject) {
-        reloadled()
-    }
-    
-    // call the API, parse the response and show result
-    private func reloadled() {
-        var request = NSMutableURLRequest(URL: NSURL(string: "https://api.spark.io/v1/devices/53ff6b066667574852212367/led")!)
+    func pressed(sender: UIButton!)
+    {
+        
+        var request = NSMutableURLRequest(URL: NSURL(string: "https://api.spark.io/v1/devices/\(device_id)/led")!)
         var session = NSURLSession.sharedSession()
         request.HTTPMethod = "POST"
-        request.HTTPBody = "access_token=94f2788badb6634bc483fc1efe541e810280b9da&params=l1,HIGH".dataUsingEncoding(NSUTF8StringEncoding)
+        request.HTTPBody = "access_token=\(access_token)&params=l1,HIGH".dataUsingEncoding(NSUTF8StringEncoding)
         
         var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
             println("Response: \(response)")
@@ -36,6 +53,19 @@ class ViewController: UIViewController {
         })
         
         task.resume()
+    }
+    
+    private func createCircle(radius: CGFloat, color: UIColor) -> UIView
+    {
+        let screenWidth = UIScreen.mainScreen().bounds.size.width
+        let screenHeight = UIScreen.mainScreen().bounds.size.height
+        let x = (screenWidth - radius) / 2.0
+        let y = (screenHeight - radius) / 2.0
         
+        let circle = UIView(frame: CGRectMake(x, y, radius, radius))
+        circle.alpha = 0.5
+        circle.layer.cornerRadius = radius / 2.0
+        circle.backgroundColor = color
+        return circle
     }
 }
