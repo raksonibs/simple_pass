d9a1eac3059c3fda6496341d29e61453be58e05c
Oskar Niburski <oskarniburski@gmail.com>
2014-11-23 01:51:51 -0500

    Updated video view

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validate :password_is_equal
 
   def password_is_equal
-    if password != '3421'
+    if password != '1364'
       errors.add(:password, 'inputted was incorrect')
     end
   end
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module SimplePass
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/videos"
   end
 end
