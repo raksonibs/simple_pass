3738cc860eb2686729337202ab8e58164816a713
Oskar Niburski <oskarniburski@gmail.com>
2014-11-24 23:09:32 -0500

    Update to spark core code

--- app/helpers/multilightsuccessfailurejustneedsservocode.ino
+++ app/helpers/multilightsuccessfailurejustneedsservocode.ino
@@ -6,25 +6,30 @@
 // give her the code via the iphone app label? she clicks the buttons via the iphone app, with one start telling her which
 // to click! No need for cards, because that is a little bit annoying
 // note that will not have off sequence, will just turn lights off after some delay in seconds and reset everything
-// need to actuall set this up now with iphone and the servo code.
-// servo will have flag for order while ihpone will have four buttons, start for servo and each corresponding light
-// failure will go off as registered here
-// need to make sure cannot click somehow when failure going off
-// would be nice to have transitions of some sort, but make rectangular start with three small circles for each button
-// and when success servo can rotate the flag to a happy face with the yellow flashing light and give her the code for the last lock on the iphone
-bool onefirst = false;
-bool twosecond = false;
-bool thirdthird = false;
+// changed to second first third
+
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+
+int pos = 0; // variable to store the servo position 
+bool runFlag = true;
+
+bool first = false;
+bool second = false;
+bool third = false;
 
 // name the pins
-int led1 = D0;
+int led1 = D5;
 int led2 = D1;
 int led3 = D2;
 int success = D3;
 int failure = D4;
+
 // This routine runs only once upon reset
 void setup()
-{
+{ 
+   myservo.attach(D0);
+   resetPosition();
      Serial.begin(9600);
    //Register our Spark function here
    Spark.function("led", ledControl);
@@ -44,16 +49,35 @@ void setup()
    digitalWrite(failure, LOW);
 }
 
+void resetPosition()
+{
+  int pos = myservo.read();
+  for (int i = pos; i > 3; i--) {
+    myservo.write(i);
+    delay(50);
+  }
+}
+
 // code is 1,2,3
 // This routine loops forever
 void loop()
 {
    // Nothing to do here
-   if (onefirst) {
-       if (twosecond) {
-           if (thirdthird) {
+   if (first) {
+       if (second) {
+           if (third) {
                Serial.println("Should turn success on!");
                digitalWrite(success, HIGH);
+               // put servo on
+               if (runFlag) {
+              for (int i = 0; i < 50; i++) {
+                delay(50);
+                myservo.write(i * (90.0 / 50.0));
+              }
+              runFlag = false;
+              resetPosition();
+              myservo.detach();
+            }
            }
         //   want to send value to change label on iphone basically
        }
@@ -70,7 +94,7 @@ void loop()
 // for example: l1,HIGH or l1,LOW
 //              l2,HIGH or l2,LOW
 // will need to reset lights if they are not working or incorrect code
-
+// second first third
 int ledControl(String command)
 {
    int state = 0;
@@ -81,39 +105,40 @@ int ledControl(String command)
    Serial.println("pin number");
    Serial.println(pinNumber);
    Serial.println("first one before l1");
-   Serial.println(onefirst);
-   if (pinNumber == 0 && onefirst == 0) {
-       onefirst = true;
+   Serial.println(first);
+//   first button is second key
+   if (pinNumber == 0 && second == 0) {
+       second = true;
        Serial.println("entering to change one");
    }
    Serial.println("first one after l1 goes on");
-   Serial.println(onefirst);
+   Serial.println(second);
    Serial.println("second one before click");
-   Serial.println(twosecond);
+   Serial.println(first);
 //   might not work because onefirst always set to false?
-   if (pinNumber == 1 && twosecond == false) {
+   if (pinNumber == 1 && first == false) {
        Serial.println("second one hopefully changing");
-       twosecond = true;
+       first = true;
    }
    Serial.println("second one after loop");
-   Serial.println(twosecond);
+   Serial.println(first);
    
    Serial.println("third one before click");
-   Serial.println(thirdthird);
+   Serial.println(third);
 //   might not work because onefirst always set to false?
-   if (pinNumber == 2 && thirdthird == false) {
+   if (pinNumber == 2 && third == false) {
        Serial.println("third one hopefully changing");
-       thirdthird = true;
+       third = true;
    }
    Serial.println("third one after loop");
-   Serial.println(thirdthird);
+   Serial.println(third);
 //   if click second button and first one is false, reset all values, and print error (will be error light) 
-   if (pinNumber == 1 && onefirst == false) {
+   if (pinNumber == 0 && first == false) {
        Serial.println("Error: Redo because clicked two and one not done");
-       twosecond = false;
-       onefirst = false;
-       thirdthird = false;
-       digitalWrite(0, 0);
+       second = false;
+       first = false;
+       third = false;
+       digitalWrite(5, 0);
        digitalWrite(1, 0);
        digitalWrite(2, 0);
        digitalWrite(4, HIGH);
@@ -131,12 +156,12 @@ int ledControl(String command)
     //   digitalWrite(3, 1); whatever errror is
    }
    
-   if (pinNumber == 2 && (onefirst == false || twosecond == false)) {
+   if (pinNumber == 2 && (first == false || second == false)) {
        Serial.println("Error: Redo because clicked three and one or two not done");
-       twosecond = false;
-       onefirst = false;
-       thirdthird = false;
-       digitalWrite(0, 0);
+       second = false;
+       first = false;
+       third = false;
+       digitalWrite(5, 0);
        digitalWrite(1, 0);
        digitalWrite(2, 0);
        digitalWrite(4, HIGH);
@@ -156,6 +181,10 @@ int ledControl(String command)
 //   if (twosecond && onefirst) {
 //       digitalWrite(D3, HIGH);
 //   }
+
+    if (pinNumber == 0) {
+        pinNumber = 5;
+    }
 
    // find out the state of the led
    if(command.substring(3,7) == "HIGH") state = 1;
